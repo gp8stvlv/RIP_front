9b0cac7d17867ee9f4f3a06b511a921d6461cb72
Grigoriy <gri003p@mail.ru>
2023-12-07 16:39:40 +0300

    м

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// add the beginning of your app entry
+import 'vite/modulepreload-polyfill'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +13,14 @@ export default defineConfig({
     //     changeOrigin: true,
     //   },
     //},
+  },
+  build: {
+    // generate .vite/manifest.json in outDir
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: '/path/to/main.js',
+    },
   },
   plugins: [react()],
   base: '/RIP_front/',
