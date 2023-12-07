53f0c8b4fc4d4899b074c5ebff8fa8e20ed73de3
Grigoriy <gri003p@mail.ru>
2023-12-07 18:08:31 +0300

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000,
-    // proxy: {
-    //   '/bouquets/': {
-    //     target: 'http://127.0.0.1:8000',
-    //     changeOrigin: true,
-    //   },
-    //},
-  },
+  // other configurations...
   build: {
-    // other build options...
-    assetsInlineLimit: 0, // Add this line to ensure module scripts are not inlined
+    assetsInlineLimit: 0,
   },
   plugins: [react()],
-  base: '/RIP_front/',
-})
+  // Remove base path temporarily
+  // base: '/RIP_front/',
+});
