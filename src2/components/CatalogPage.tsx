279b3e803512385e25266376d171b32f051eced1
Grigoriy <gri003p@mail.ru>
2023-12-11 13:12:20 +0300

    catalog.tsx

--- src2/components/CatalogPage.tsx
+++ src2/components/CatalogPage.tsx
@@ -1,9 +1,8 @@
-import { FC, useState, useEffect } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import Breadcrumbs from './Breadcrumbs';
 import './Catalog.css';
 import logoImage from './CH.png';
-import React from 'react';
 
 // Определение типа для объекта "Букет"
 interface chemistryEquipment {
@@ -16,10 +15,28 @@ interface chemistryEquipment {
     image_url_after_serializer: string;
 }
 
-// В функциональных компонентах для работы с состоянием можно использовать хук useState()
-// Он возвращает кортеж из двух элементов:
-// 1 элемент - объект состояния
-// 2 элемент - метод который позволит нам обновить состояние
+// Mock data for chemistryEquipment
+const chemistryEquipmentMocks: chemistryEquipment[] = [
+    {
+        chemistry_product_id: 1,
+        type: 'Test Equipment 1',
+        description: 'Description for Test Equipment 1',
+        image_url: 'https://example.com/image1.jpg',
+        price: '100',
+        status: 'In Stock',
+        image_url_after_serializer: 'https://example.com/image1_after_serializer.jpg',
+    },
+    {
+        chemistry_product_id: 2,
+        type: 'Test Equipment 2',
+        description: 'Description for Test Equipment 2',
+        image_url: 'https://example.com/image2.jpg',
+        price: '150',
+        status: 'Out of Stock',
+        image_url_after_serializer: 'https://example.com/image2_after_serializer.jpg',
+    },
+    // Add more mock objects as needed
+];
 
 // Компонент "Catalog" представляет собой функциональный компонент React
 const CatalogPage: FC = () => {
@@ -33,7 +50,7 @@ const CatalogPage: FC = () => {
     const priceParam = queryParams.get('price') || '';
 
     // Использование хука useState для управления состоянием компонента
-    const [equipments, setEquipment] = useState<chemistryEquipment[]>([]);
+    const [equipments, setEquipment] = useState<chemistryEquipment[]>(chemistryEquipmentMocks);
     const [searchValue, setSearchValue] = useState(searchParam);
     const [priceValue, setPriceValue] = useState(priceParam);
 
@@ -44,7 +61,7 @@ const CatalogPage: FC = () => {
         console.log('Fetching from:', apiUrl);
 
         fetch(apiUrl)
-            .then(response => {
+            .then((response) => {
                 console.log('Response status:', response.status);
 
                 if (!response.ok) {
@@ -53,19 +70,17 @@ const CatalogPage: FC = () => {
 
                 return response.json();
             })
-            .then(data => {
+            .then((data) => {
                 console.log('Data received:', data);
                 setEquipment(data);
             })
-            .catch(error => {
+            .catch((error) => {
                 console.error('Error fetching ChemistryEquipments:', error);
             });
     };
 
     // Массив объектов для построения breadcrumbsItems
-    const breadcrumbsItems = [
-        { label: 'Каталог', link: '' } // Ссылка на текущую страницу
-    ];
+    const breadcrumbsItems = [{ label: 'Каталог', link: '' }]; // Ссылка на текущую страницу
 
     // Обработчик события для кнопки поиска
     const handleSearchClick = () => {
@@ -93,14 +108,14 @@ const CatalogPage: FC = () => {
                             id="search-input"
                             placeholder="Поиск"
                             value={searchValue}
-                            onChange={(event => setSearchValue(event.target.value))}
+                            onChange={(event) => setSearchValue(event.target.value)}
                         />
                         <input
                             type="number"
                             id="price-input"
                             placeholder="Цена"
                             value={priceValue}
-                            onChange={(event => setPriceValue(event.target.value))}
+                            onChange={(event) => setPriceValue(event.target.value)}
                         />
                         <button type="button" id="search-button" onClick={handleSearchClick}>
                             Искать
@@ -111,7 +126,12 @@ const CatalogPage: FC = () => {
                         <div className="col-4" key={chemistryEquipment.chemistry_product_id}>
                             <div className="card">
                                 <img
-                                    src={(chemistryEquipment.image_url_after_serializer != '' && chemistryEquipment.image_url_after_serializer !== 'http://localhost:9000/images/images/None') ? chemistryEquipment.image_url_after_serializer : logoImage}
+                                    src={
+                                        chemistryEquipment.image_url_after_serializer !== '' &&
+                                            chemistryEquipment.image_url_after_serializer !== 'http://localhost:9000/images/images/None'
+                                            ? chemistryEquipment.image_url_after_serializer
+                                            : logoImage
+                                    }
                                     alt={chemistryEquipment.type}
                                     className="card-img-top"
                                 />
@@ -121,10 +141,13 @@ const CatalogPage: FC = () => {
                                     <p className="card-text">Цена: {chemistryEquipment.price} рублей</p>
                                     {/* Add more text elements here if needed */}
 
-                                    <a href={`/EquipmentDetail/${chemistryEquipment.chemistry_product_id}/`} className="btn btn-primary" style={{ height: '40px' }}>
+                                    <a
+                                        href={`/EquipmentDetail/${chemistryEquipment.chemistry_product_id}/`}
+                                        className="btn btn-primary"
+                                        style={{ height: '40px' }}
+                                    >
                                         Подробнее
                                     </a>
-
                                 </div>
                             </div>
                         </div>
