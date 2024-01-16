84d5efa21b763f752583f65b153626117219bcda
Grigoriy <gri003p@mail.ru>
2024-01-17 01:14:22 +0300

    complited github pages

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  // other configurations...
+  build: {
+    assetsInlineLimit: 0,
+  },
+  base: "",
+  plugins: [react()],
+});
